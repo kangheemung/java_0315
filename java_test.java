9d3d8ec8b03375449766a12ecdff9e661445a66b
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-16 18:24:00 +0900

    add

--- java_test.java
+++ java_test.java
@@ -1,12 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        int count=0;
-        for (int i = 1; i <= 3; i++) {
-                for (int j = 0; j < i; j++) { // 行数分の数字を出力するループ
-                System.out.print("0");
+class Advanced6 {
+    public static void main(String [] args) {
+
+        int width = 5;
+        int height = 5;
+        int start = (width - 1) / 2;
+        int end = start;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(j >= start && j <= end ? 0 : " ");
             }
-             System.out.println();
+
+            System.out.print("\n");
+
+            if (i < (height - 1) / 2) {
+                start --;
+                end ++;
+            } else {
+                start ++;
+                end --;
+            }
         }
     }
 }
-
