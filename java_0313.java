b9037451d575b5f065144c0eb944faf05496e8d7
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-16 17:24:35 +0900

    test

--- java_0313.java
+++ java_0313.java
@@ -127,3 +127,11 @@ class Sample2 {
         }
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i+"個");
+        }
+    }
+}
---
+++ java_test.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int count=0;
+        for (int i = 1; i <= 3; i++) {
+                for (int j = 0; j < i; j++) { // 行数分の数字を出力するループ
+                System.out.print("0");
+            }
+             System.out.println();
+        }
+    }
+}
+
