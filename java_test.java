fb670ff372651156f80be2fee2840f8f9f889d22
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-17 11:16:24 +0900

    add

--- java_test.java
+++ java_test.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 public class CalculatorTest {
     @test
     public void totalSum() {
-         Calculator calculator = new Calculator();
+         Calculator sut = new Calculator();
+        // 想定する計算結果の用意
         int expected = 5;
-
-        // Act
-        int result = calculator.sum(3, 2);
-
-        // Assert
-        assertEquals(expected, result);
+        // 3 + 2の結果を`sum()`に計算させる
+        int actual = sut.sum(3, 2);
+        // 計算結果が5となるか検証する
+        assertThat(actual).isEqualTo(expected);
 
     }
 }
